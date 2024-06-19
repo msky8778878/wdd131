329828b52e920d359d8a0e28bde1a6ea57975f2e
msky8778878 <michalskymi@gmail.com>
2024-06-19 09:46:42 +0200

    week 2 part 4

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,14 +2,25 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button')
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
 
-const deleteButton = document.createElement('button');
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
 
-li.textContent = input.value;
+        input.value = '';
+        input.focus();
 
-deleteButton.textContent = '❌';
-
-li.append(deleteButton);
-
-list.append(li);
+    } else {
+        alert("Please enter text!")
+        input.focus();
+    }
+});
